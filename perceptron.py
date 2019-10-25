1bd058ffe98b831227791c668a4fc12898f22a64
Chad Bowman <chad.bowman0@gmail.com>
2019-10-24 23:08:04 -0600

    improve styling

--- perceptron.py
+++ perceptron.py
@@ -9,7 +9,7 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 
-def generate_weights(tup = (3, 1)):
+def generate_weights(tup=(3, 1)):
     return 2 * np.random.random(tup) - 1
 
 
@@ -19,24 +19,27 @@ def update_weights(weights, inputs, outputs, targets):
     return weights + np.dot(inputs.T, adjustments)
 
 
+def activation(inputs, weights):
+    return sigmoid(np.dot(inputs, weights))
+
+
 def train(training_set, targets, weights=None, iterations=1000):
-    
+
     weights = generate_weights((training_set.shape[1], 1))
 
     for interations in range(iterations):
-        model = lambda x: sigmoid(np.dot(x, weights))
-        result = model(training_set)
+        result = activation(training_set, weights)
         weights = update_weights(weights, training_set, result, targets)
 
-    return model 
+    return lambda x: activation(x, weights)
 
 
 if __name__ == '__main__':
     training_inputs = np.array([
-        [0, 0, 1], # 0
-        [1, 1, 1], # 1
-        [1, 0, 1], # 1
-        [0, 1, 1]  # 0
+        [0, 0, 1],  # 0
+        [1, 1, 1],  # 1
+        [1, 0, 1],  # 1
+        [0, 1, 1]   # 0
     ])
 
     training_outputs = np.array([
@@ -44,7 +47,7 @@ if __name__ == '__main__':
     ]).T
 
     perceptron = train(training_inputs, training_outputs)
-    
+
     x = np.array([1, 0, 0])
     result = perceptron(x)[0]
 
