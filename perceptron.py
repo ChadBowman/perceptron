c960188dde295b5699fed8e92cd185969c67940a
Chad Bowman <chad.bowman0@gmail.com>
2019-11-13 00:04:12 -0700

    allow activation to be generic, make into iterator, docstrings

--- perceptron.py
+++ perceptron.py
@@ -1,54 +1,89 @@
-import numpy as np
+from numpy import dot, random, array, mean
+from sympy import lambdify, symbols, E
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+class Perceptron:
+    """A single perceptron with a fixed number of inputs"""
 
+    def __init__(self, *, activation, training_set, targets, tolerance=0.01):
+        """Initializes partial derivaties and randomizes weights
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+        Keyword arguments:
+        activation -- the activation function
+        training_set -- nxm matrix of inputs with known solutions
+        targets -- n vector representing solution to training_set
+        tolerance -- lowest possible error before terminating training
+                     default: 0.01
+        """
+        self.activation = lambdify(activation.free_symbols, activation)
+        self.training_set = training_set
+        self.targets = targets
+        self.tolerance = tolerance
 
+        self.partials = {}
+        for sym in activation.free_symbols:
+            self.partials[sym] = lambdify(sym, activation.diff(sym))
 
-def generate_weights(tup=(3, 1)):
-    return 2 * np.random.random(tup) - 1
+    def __call__(self, inputs):
+        """Invokes the perceptron to make a prediction
 
+        inputs -- n vector of inputs
+        """
+        product = dot(inputs, self.weights)
+        return self.activation(product)
 
-def update_weights(weights, inputs, outputs, targets):
-    error = targets - outputs
-    adjustments = error * sigmoid_derivative(outputs)
-    return weights + np.dot(inputs.T, adjustments)
+    def __iter__(self):
+        """Resets weights and returns itself"""
+        n = len(self.training_set[0])
+        self.weights = random.random((n, 1)) - 1
+        return self
 
+    def __next__(self):
+        """Takes a step through the training iteration.
+        Raises StopIteration when all errors are below tolerance
+        """
+        guess = self(self.training_set)
+        error = self.targets - guess
 
-def activation(inputs, weights):
-    return sigmoid(np.dot(inputs, weights))
+        if all(self.tolerance > error):
+            raise StopIteration
 
+        adjustments = []
+        for partial in self.partials.values():
+            adjustments.append(error * partial(guess))
 
-def train(training_set, targets, weights=None, iterations=1000):
+        self.weights += dot(self.training_set.T, sum(adjustments))
 
-    weights = generate_weights((training_set.shape[1], 1))
+        return error
 
-    for interations in range(iterations):
-        result = activation(training_set, weights)
-        weights = update_weights(weights, training_set, result, targets)
-
-    return lambda x: activation(x, weights)
+    def train(self):
+        """Fully trains perceptron and returns self"""
+        for error in self:
+            pass
+        return self
 
 
 if __name__ == '__main__':
-    training_inputs = np.array([
+    training_inputs = array([
         [0, 0, 1],  # 0
         [1, 1, 1],  # 1
         [1, 0, 1],  # 1
         [0, 1, 1]   # 0
     ])
 
-    training_outputs = np.array([
+    training_outputs = array([
         [0, 1, 1, 0]
     ]).T
 
-    perceptron = train(training_inputs, training_outputs)
+    x = symbols('x')
+    sig = 1 / (1 + E**(-x))
 
-    x = np.array([1, 0, 0])
-    result = perceptron(x)[0]
+    harbinger = Perceptron(
+        activation=sig,
+        training_set=training_inputs,
+        targets=training_outputs
+    )
 
-    print(f'{x} : {np.round(result)}')
+    harbinger.train()
+
+    print(harbinger(array([1, 0, 0])))
